95ea1c9c9d809968025d04d03b6ddd71876d0d24
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2025-04-14 18:19:05 +0100

    Add functionality to delete button to delete task

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,12 @@ export default function App() {
     })
   }
 
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -45,7 +51,7 @@ export default function App() {
               <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
           </li>
         })}
       </ul>
